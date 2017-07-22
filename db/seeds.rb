509f5ceba249679e7afe08b2ec5663a83846e638
tri wantoro <ajarsinau@gmail.com>
2017-07-22 07:40:25 +0700

    active admin dan active skin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(email: 'ajarsinau@gmail.com', password: 'bismillah12', password_confirmation: 'bismillah12') if Rails.env.development?
