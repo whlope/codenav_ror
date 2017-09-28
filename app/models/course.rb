77101c4a2ad305bab458bce1b26eb6401b972fb9
tri wantoro <ajarsinau@gmail.com>
2017-09-28 09:34:21 +0700

    Cloudinary

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,14 +4,14 @@ class Course < ApplicationRecord
 	has_many :tasks, -> { order(position: :asc) }, dependent: :destroy
 	has_many :subscriptions, dependent: :destroy
 	has_many :reviews, dependent: :destroy
-	
+
 	validates :title, presence: true, length: {maximum: 50}
 	validates :description, presence: true
 	validates :price, presence: true, numericality: true
 
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
-	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-
+	#has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+	#validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+	mount_uploader :image, ImageUploader
 	def average_rating
 		reviews.blank? ? 0 : reviews.average(:star).round(2)
 	end
